504c5670dda1d01e7be12104fc84558177fddfcb
darya-startsava <daryastartsava@gmail.com>
2022-02-08 13:00:25 +0100

    fix: move footer to bottom

--- src/ts/components/footer/footer.ts
+++ src/ts/components/footer/footer.ts
@@ -2,6 +2,7 @@ import template from 'lodash.template';
 import Component from '../abstract-component';
 import HeaderHTML from './footer.html';
 import './footer.scss';
+import * as constants from '../../constants';
 
 export default class Footer extends Component {
   constructor() {
@@ -9,6 +10,9 @@ export default class Footer extends Component {
   }
   render(): HTMLElement {
     this.container.innerHTML = template(HeaderHTML)();
+    if (localStorage.getItem(constants.currentRoute) === 'textbook') {
+      this.container.classList.add('textbook-footer');
+    }
     return this.container;
   }
 }
