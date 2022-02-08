24a96d90b5b26a39df65a6494c11cc86ec2414ee
darya-startsava <daryastartsava@gmail.com>
2022-02-08 01:44:04 +0100

    feat: add content to main page

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './ts/router';
+import './custom.scss';
 
 window.addEventListener('load', router);
--- src/ts/pages/main/main.ts
+++ src/ts/pages/main/main.ts
@@ -1,8 +1,9 @@
 import template from 'lodash.template';
 import Header from '../../components/header/header';
-import Registration from '../../components/registration/registration';
-import Login from '../../components/login/login';
+// import Registration from '../../components/registration/registration';
+// import Login from '../../components/login/login';
 import MainHTML from './main.html';
+import './main.scss';
 
 export default function bootstrap(): void {
   const body = document.querySelector<HTMLElement>('body');
@@ -12,8 +13,9 @@ export default function bootstrap(): void {
   const header = new Header();
   body?.append(header.render());
   const main = document.createElement('main');
+  main.classList.add('main');
   main.innerHTML = template(MainHTML)();
   body?.append(main);
-  main.append(new Registration().render());
-  main.append(new Login().render());
+  //   main.append(new Registration().render());
+  //   main.append(new Login().render());
 }
