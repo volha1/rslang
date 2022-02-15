433cb0c59735d1f55c34729a92d5cdee1534adcf
volha1 <71kiss@gmail.com>
2022-02-15 20:09:40 +0100

    feat: fix audio play in game results

--- src/ts/components/game-result/game-result.ts
+++ src/ts/components/game-result/game-result.ts
@@ -2,6 +2,7 @@ import template from 'lodash.template';
 import Component from '../abstract-component';
 import GameResultHTML from './game-result.html';
 import state from '../../state';
+import * as constants from '../../constants';
 
 export default class GameResult extends Component {
   constructor() {
@@ -10,6 +11,18 @@ export default class GameResult extends Component {
 
   render(): HTMLElement {
     this.container.innerHTML = template(GameResultHTML)({ rightAnswers: state.gameRightAnswers, wrongAnswers: state.gameWrongAnswers });
+    this.addListeners();
     return this.container;
+  }
+
+  private addListeners(): void {
+    this.container.querySelectorAll<HTMLElement>('.result-audio-btn').forEach((item) => {
+      item.addEventListener('click', (event) => {
+        const audioFile = (<HTMLButtonElement>event.currentTarget).dataset.audio;
+        const wordAudio = new Audio(`${constants.url}${audioFile}`);
+        wordAudio.play();
+        }
+      );
+    });
   }
 }
