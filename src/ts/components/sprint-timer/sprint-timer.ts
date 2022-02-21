3d8c5551041629a4a4340b4ce386f5b0958484c9
darya-startsava <daryastartsava@gmail.com>
2022-02-22 00:37:59 +0100

    feat: add timer

--- src/ts/components/sprint-timer/sprint-timer.ts
+++ src/ts/components/sprint-timer/sprint-timer.ts
@@ -2,8 +2,6 @@ import template from 'lodash.template';
 import Component from '../abstract-component';
 import SprintTimerHTML from './sprint-timer.html';
 import './sprint-timer.scss';
-import store from '../../store';
-import * as constants from '../../constants';
 
 export default class SprintTimer extends Component {
   constructor() {
@@ -11,8 +9,21 @@ export default class SprintTimer extends Component {
   }
   render(): HTMLElement {
     this.container.innerHTML = template(SprintTimerHTML)();
-    this.addListeners();
+    let sec = 60;
+    const intervalId = setInterval(() => {
+      const timeInTImer = this.container.querySelector('.time-in-timer');
+      sec -= 1;
+      if (timeInTImer) {
+        if (sec > 9) {
+          timeInTImer.innerHTML = `${sec}`;
+        } else {
+          timeInTImer.innerHTML = `0${sec}`;
+        }
+      }
+      if (!sec) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
     return this.container;
   }
-  addListeners(): void {}
 }
